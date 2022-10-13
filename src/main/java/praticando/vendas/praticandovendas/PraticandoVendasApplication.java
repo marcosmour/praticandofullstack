79ee78d0b014fcff91d31f510bb6fa90f2d4b0cd
Marcos Moura <marcosmoura201@hotmail.com>
2022-10-13 09:25:29 -0300

    Criando o projeto

---
+++ src/main/java/praticando/vendas/praticandovendas/PraticandoVendasApplication.java
@@ -0,0 +1,13 @@
+package praticando.vendas.praticandovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandoVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/praticando/vendas/praticandovendas/PraticandoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package praticando.vendas.praticandovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
