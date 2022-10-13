643e80b5e1bb653f0128069453b30dc01a2c1887
Marcos Moura <marcosmoura201@hotmail.com>
2022-10-13 13:01:11 -0300

    Testando a aplicacao

--- src/main/java/praticando/vendas/praticandovendas/PraticandoVendasApplication.java
+++ src/main/java/praticando/vendas/praticandovendas/PraticandoVendasApplication.java
@@ -2,9 +2,18 @@ package praticando.vendas.praticandovendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PraticandoVendasApplication {
+	
+	@GetMapping("/hello/{nome}")
+	public String hello(@PathVariable String nome) {
+		return "Hello " + nome;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PraticandoVendasApplication.class, args);
